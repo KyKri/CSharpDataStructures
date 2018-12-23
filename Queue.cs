e617a49abcf5f1bc76d5624696edb63ee3d21c6e
Kyle Krick <krkrick@gmail.com>
2018-12-22 16:54:03 -0800

    stubbed out Queue class

--- Queue.cs
+++ Queue.cs
@@ -1,8 +1,50 @@
 using System;
 
-namespace DataStructures {
+namespace DataStructures
+{
 
-    class Queue<E>{
-        
+    class Queue<E>
+    {
+        //Initializes new empty queue of desiredSize
+        public Queue(int desiredSize)
+        {
+
+        }
+
+        //Attempts to add specified item, returns true if successful, false otherwise
+        public bool Enqueue()
+        {
+            return false;
+        }
+
+        //Attempts to Dequeue the specified item, returns null if unable to find item
+        public E Dequeue(E item)
+        {
+            return default(E);
+        }
+
+        //Returns the Queue to an empty state with size of desiredSize
+        public void Empty()
+        {
+
+        }
+
+        //Returns current size of the Queue
+        public int GetSize()
+        {
+            return 0;
+        }
+
+        //Returns true if the Queue is full
+        public bool IsFull()
+        {
+            return false;
+        }
+
+        //Returns true if the Queue is empty
+        public bool IsEmpty()
+        {
+            return false;
+        }
     }
 }

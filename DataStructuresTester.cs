65e8e8b6247a90b752e44c71e6084b99cc895d69
Kyle Krick <krkrick@gmail.com>
2018-12-27 01:01:02 -0600

    added comments to document tests, added tests for Contains

--- DataStructuresTester.cs
+++ DataStructuresTester.cs
@@ -15,14 +15,22 @@ namespace DataStructures{
 
             Queue<int> q = new Queue<int>(1);
 
+            //tests on empty queue
             Console.WriteLine($"GetSize should be 0: {q.GetSize()}");
             Console.WriteLine($"IsFull should be false: {q.IsFull()}");
             Console.WriteLine($"IsEmpty should be true: {q.IsEmpty()}");
+
+            //tests on full queue
             Console.WriteLine($"Enqueue should be true: {q.Enqueue(1)}");
             Console.WriteLine($"GetSize should be 1: {q.GetSize()}");
             Console.WriteLine($"IsFull should be true: {q.IsFull()}");
             Console.WriteLine($"IsEmpty should be false: {q.IsEmpty()}");
             Console.WriteLine($"Enqueue should be false: {q.Enqueue(1)}");
+
+            //testing contains on populated queue
+            //TO-DO: add tests for Queue that has nothing in first slot (0th element)
+            Console.WriteLine($"Contains should be true: {q.Contains(1)}");
+            Console.WriteLine($"Contains should be false: {q.Contains(0)}");
         }
     }
 }

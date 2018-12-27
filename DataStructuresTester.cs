cf36eb665d6301a2d99dabffcefec7b9fc4c58fc
Kyle Krick <krkrick@gmail.com>
2018-12-27 00:31:14 -0600

    Added tests to verify methods with a non-empty queue

--- DataStructuresTester.cs
+++ DataStructuresTester.cs
@@ -18,6 +18,11 @@ namespace DataStructures{
             Console.WriteLine($"GetSize should be 0: {q.GetSize()}");
             Console.WriteLine($"IsFull should be false: {q.IsFull()}");
             Console.WriteLine($"IsEmpty should be true: {q.IsEmpty()}");
+            Console.WriteLine($"Enqueue should be true: {q.Enqueue(1)}");
+            Console.WriteLine($"GetSize should be 1: {q.GetSize()}");
+            Console.WriteLine($"IsFull should be true: {q.IsFull()}");
+            Console.WriteLine($"IsEmpty should be false: {q.IsEmpty()}");
+            Console.WriteLine($"Enqueue should be false: {q.Enqueue(1)}");
         }
     }
 }

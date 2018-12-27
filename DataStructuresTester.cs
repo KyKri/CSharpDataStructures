10519b7bb7af026eef232e72bed65186d381f969
Kyle Krick <krkrick@gmail.com>
2018-12-26 23:38:16 -0600

    added tests for constructor, GetSize, IsFull and IsEmpty

--- DataStructuresTester.cs
+++ DataStructuresTester.cs
@@ -12,6 +12,12 @@ namespace DataStructures{
         public void TestQueue()
         {
             Console.WriteLine("DataStructuresTester: Testing Queue");
+
+            Queue<int> q = new Queue<int>(1);
+
+            Console.WriteLine($"GetSize should be 0: {q.GetSize()}");
+            Console.WriteLine($"IsFull should be false: {q.IsFull()}");
+            Console.WriteLine($"IsEmpty should be true: {q.IsEmpty()}");
         }
     }
 }
